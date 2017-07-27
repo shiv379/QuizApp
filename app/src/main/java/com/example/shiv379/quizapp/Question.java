bfb75d9e286c750619b9bfe96cabcc9b2a5841b7
Toby King <shiv379@gmail.com>
2017-07-27 12:40:48 +0100

    Can't even remember what I did now. A whole bunch of stuff.

--- app/src/main/java/com/example/shiv379/quizapp/Question.java
+++ app/src/main/java/com/example/shiv379/quizapp/Question.java
@@ -1,78 +1,91 @@
 package com.example.shiv379.quizapp;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
  * Created by Toby on 22/07/2017.
+ *
+ * {@Question} represents a question of type text, multiple choice, or single choice from multiple
+ *
  */
 
-public class Question {
+class Question {
 
-    public static final int QUESTION_TYPE_TEXT = 1;
-    public static final int QUESTION_TYPE_MULTIPLE = 2;
-    public static final int QUESTION_TYPE_RADIO = 3;
-    private int mQuestionType;
-    private int mImageResource;
-    private String mQuestionText;
-    private ArrayList<String> mAnswers = new ArrayList<String>();
-    private ArrayList<Integer> mCorrectAnswers = new ArrayList<Integer>();
+    static final int QUESTION_TYPE_TEXT = 1;
+    static final int QUESTION_TYPE_MULTIPLE = 2;
+    static final int QUESTION_TYPE_RADIO = 3;
+    private final int mQuestionType;
+    private final String mQuestionText;
+    private ArrayList<String> mAnswers = new ArrayList<>(); //List of possible answers
+    private ArrayList<Integer> mCorrectAnswers = new ArrayList<>(); //list of index ids from mAnswers for correct answers
 
-    //Multiple choice, no image
-    public Question(String questionText, String[] answers, Integer[] correctAnswers) {
+    /**
+     *Multiple choice, no image
+     * @param questionText is the text of the question
+     * @param answers is the list of potential answers
+     * @param correctAnswers is the list of index ids from the answers array indicating which are correct answers
+     */
+    Question(String questionText, String[] answers, Integer[] correctAnswers) {
         mQuestionType = QUESTION_TYPE_MULTIPLE;
         mQuestionText = questionText;
-        mAnswers = new ArrayList<String>(Arrays.asList(answers));
-        mCorrectAnswers= new ArrayList<Integer>(Arrays.asList(correctAnswers));
+        mAnswers = new ArrayList<>(Arrays.asList(answers));
+        mCorrectAnswers= new ArrayList<>(Arrays.asList(correctAnswers));
     }
 
-    //Radio choice, no image
-    public Question(String questionText, String[] answers, Integer correctAnswers) {
+    /**
+     * Radio choice, no image
+     * @param questionText is the text of the question
+     * @param answers is the list of potential answers
+     * @param correctAnswers is the index ids from the answers array indicating which is the correct answer
+     */
+    Question(String questionText, String[] answers, Integer correctAnswers) {
         mQuestionType = QUESTION_TYPE_RADIO;
         mQuestionText = questionText;
-        mAnswers = new ArrayList<String>(Arrays.asList(answers));
-        mCorrectAnswers= new ArrayList<Integer>(Arrays.asList(correctAnswers));
+        mAnswers = new ArrayList<>(Arrays.asList(answers));
+        mCorrectAnswers= new ArrayList<>(Arrays.asList(correctAnswers));
     }
 
-    //Text question, no image, no correct answer
-    public Question(String questionText) {
+    /**
+     * Text question, no image, no correct answer
+     * @param questionText is the text of the question
+     */
+    Question(String questionText) {
         mQuestionType = QUESTION_TYPE_TEXT;
         mQuestionText = questionText;
     }
 
-    //Text question, no image, correct answer
-    public Question(String questionText, String answers) {
+    /**
+     * Text question, no image, correct answer
+     * @param questionText is the text of the question
+     * @param answers is the correct answer
+     */
+    Question(String questionText, String answers) {
         mQuestionType = QUESTION_TYPE_TEXT;
         mQuestionText = questionText;
         mAnswers.add(answers);
         mCorrectAnswers.add(0);
     }
 
-    public int getQuestionType() {
+    int getQuestionType() {
         return mQuestionType;
     }
 
-    public int getImageResource() {
-        return mImageResource;
-    }
-
-    public String getQuestionText() {
+    String getQuestionText() {
         return mQuestionText;
     }
 
-    public ArrayList<String> getAnswers() {
+    ArrayList<String> getAnswers() {
         return mAnswers;
     }
 
-    public ArrayList<Integer> getCorrectAnswers() {
+    ArrayList<Integer> getCorrectAnswers() {
         return mCorrectAnswers;
     }
 
-    public Integer getAnswerIndexByString(String answerString) {
+    Integer getAnswerIndexByString(String answerString) {
         for (int i = 0; i < mAnswers.size(); i++){
-            if (answerString == mAnswers.get(i)){
+            if (answerString.toLowerCase().equals(mAnswers.get(i).toLowerCase())){
                 return i;
             }
         }
--- app/src/main/java/com/example/shiv379/quizapp/Quiz.java
+++ app/src/main/java/com/example/shiv379/quizapp/Quiz.java
@@ -2,8 +2,6 @@ package com.example.shiv379.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,28 +13,42 @@ import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
+
+/**
+ * Created by Toby on 22/07/2017.
+ *
+ * Quiz app created for Udacity Android Developer nanodegree
+ * Proof of concept using different UI elements. I got a bit carried away and
+ * made it much more complex than the original brief!
+ *
+ * If developed further I will look into using fragments and adapters to allow
+ * better styling of the UI elements.
+ */
 
 public class Quiz extends AppCompatActivity {
 
-    final ArrayList<Question> mQuestions = new ArrayList<Question>();
+    private final ArrayList<Question> mQuestions = new ArrayList<>();
     private int mQuestionIndex = 0;
-    private ArrayList<Integer> mQuestionViews = new ArrayList<Integer>();
+    private ArrayList<Integer> mQuestionViews = new ArrayList<>();
+    private int mScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        if (savedInstanceState != null) {
+            mQuestionIndex = savedInstanceState.getInt("question_index", 0);
+            mScore = savedInstanceState.getInt("score",0);
+        }
+
         mQuestions.add(new Question("What is your name?")); //Text input, no correct answer
         mQuestions.add(new Question("What programming language is this course teaching you?","Java"));
         mQuestions.add(new Question("What is the capital of Australia?",new String[]{"Sydney","Perth","Perranporth","Canberra"},3));
         mQuestions.add(new Question("What colours do you mix to make purple?",new String[]{"Red","Blue","Green","Yellow","Black","Gold"},new Integer[]{0,1}));
 
-        Button submitButton = (Button) findViewById(R.id.button);
+        Button submitButton = findViewById(R.id.button);
         submitButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 checkAnswer();
@@ -46,11 +58,15 @@ public class Quiz extends AppCompatActivity {
         doQuestion(mQuestions.get(mQuestionIndex));
     }
 
+    /**
+     *
+     * @param question presents the specified question on screen
+     */
     private void doQuestion(Question question){
         int mAnswerViewId;
         removeOldViews();
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.question_root);
-        TextView questionTextView = (TextView) findViewById(R.id.question_text);
+        LinearLayout rootView = findViewById(R.id.question_root);
+        TextView questionTextView = findViewById(R.id.question_text);
         questionTextView.setText(question.getQuestionText());
 
         RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
@@ -64,7 +80,7 @@ public class Quiz extends AppCompatActivity {
                 rootView.addView(uiItem,params);
                 mQuestionViews.add(mAnswerViewId);
                 rootView.clearFocus();
-                findViewById(R.id.button).requestFocus();
+                uiItem.requestFocus();
                 break;
             case Question.QUESTION_TYPE_RADIO:
                 RadioGroup radioGroup = new RadioGroup(this);
@@ -96,13 +112,25 @@ public class Quiz extends AppCompatActivity {
         }
     }
 
+    /**
+     * Proceed to next question, or end the quiz if out of questions
+     */
     private void nextQuestion(){
         if (mQuestionIndex < mQuestions.size()-1) {
             mQuestionIndex++;
             doQuestion(mQuestions.get(mQuestionIndex));
+        } else {
+            removeOldViews();
+            Button btn = findViewById(R.id.button);
+            ((ViewGroup) btn.getParent()).removeView(btn);
+            TextView questionTextView = findViewById(R.id.question_text);
+            questionTextView.setText(getString(R.string.score) + " " + mScore);
         }
     }
 
+    /**
+     * Checks if answer provided by the user is correct, provides Toast feedback
+     */
     private void checkAnswer(){
         Question question = mQuestions.get(mQuestionIndex);
         ArrayList<Integer> correctAnswers = question.getCorrectAnswers();
@@ -116,15 +144,15 @@ public class Quiz extends AppCompatActivity {
 
         switch (question.getQuestionType()) {
             case Question.QUESTION_TYPE_TEXT:
-                EditText editText = (EditText) findViewById(mQuestionViews.get(0));
-                if (editText.getText().toString() != question.getAnswers().get(0)){
+                EditText editText = findViewById(mQuestionViews.get(0));
+                if (!(editText.getText().toString().toLowerCase().equals(question.getAnswers().get(0).toLowerCase()))){
                     correct = false;
                 }
                 break;
             case Question.QUESTION_TYPE_RADIO:
                 for (int i : mQuestionViews) {
                     if (findViewById(i) instanceof RadioButton){
-                        RadioButton  rb  = (RadioButton) findViewById(i);
+                        RadioButton  rb  = findViewById(i);
                         int j = question.getAnswerIndexByString((String) rb.getText());
                         if ((!correctAnswers.contains(j) && rb.isChecked()) || (correctAnswers.contains(j) && !rb.isChecked())){
                             correct = false;
@@ -136,7 +164,7 @@ public class Quiz extends AppCompatActivity {
             case Question.QUESTION_TYPE_MULTIPLE:
                 for (int i : mQuestionViews) {
                     if (findViewById(i) instanceof CheckBox){
-                        CheckBox cb = (CheckBox) findViewById(i);
+                        CheckBox cb = findViewById(i);
                         int j = question.getAnswerIndexByString((String) cb.getText());
                         if ((!correctAnswers.contains(j) && cb.isChecked()) || (correctAnswers.contains(j) && !cb.isChecked())){
                             correct = false;
@@ -151,17 +179,28 @@ public class Quiz extends AppCompatActivity {
 
         if (correct){
             Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show();
+            mScore++;
         } else {
             Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT).show();
         }
         nextQuestion();
     }
 
+    /**
+     * Removes the UI elements from the previous question, by deleting everything with an id found in mQuestionViews
+     */
     private void removeOldViews(){
         for (int rId : mQuestionViews) {
             View toDel = findViewById(rId);
             ((ViewGroup) toDel.getParent()).removeView(toDel);
         }
         mQuestionViews.clear();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("question_index",mQuestionIndex);
+        outState.putInt("score",mScore);
     }
 }
